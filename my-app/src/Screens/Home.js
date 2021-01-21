810a852d30ca176d8e15371529d6e43ce8b4c0a3
h26k2 <hasnain.karim262@outlook.com>
2021-01-21 20:54:24 +0500

    my custom changing

---
+++ my-app/src/Screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Home extends React.Component{
+    render(){
+        return(
+            <h1>welcome to homepage</h1>
+        )
+    }
+}
+
+export default Home;
