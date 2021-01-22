396926bb9f4c4a4d9942336d1550833ad92bace4
h26k2 <hasnain.karim262@outlook.com>
2021-01-22 22:52:51 +0500

    some updates

--- my-app/src/Components/Footer.js
+++ my-app/src/Components/Footer.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 class Footer extends React.Component{
     render(){
         return(
             <footer className="container-fluid">
-                <span>contact us</span>
+                <span>contact on</span>
                 <div className="container">
                     <div className="row">
                         <div className="col-12">
-                            <i className="far fa-envelope"></i>
-                            <i className="far fa-comment-dots"></i>
-                            <i className="fab fa-skype"></i>
-                            <i className="far fa-paper-plane"></i>
+                            <Link to="/"><i className="far fa-envelope fa-2x"></i></Link>
+                            <Link to="/"><i className="far fa-comment-dots fa-2x"></i></Link>
+                            <Link to="/"><i className="fab fa-skype fa-2x"></i></Link>
+                            <Link to="/"><i className="far fa-paper-plane fa-2x"></i></Link>
                         </div>
                         <div className="col-12">
-                            <p>for any order related issues please email us on ayha9n@gmail.com</p>
+                            <p>For any order related issues please email us on ayha9n@gmail.com</p>
                         </div>
                         <div className="col-12">
                             <h6>copyright 2021 - buyyoutubviews.com - all rights reserved</h6>
